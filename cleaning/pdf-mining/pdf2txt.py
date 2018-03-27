28700a5f873bc3294c7ebe65aca34ab28d8c4c6e
joddiy <joddiy@qq.com>
2018-03-27 20:32:32 +0800

    * finish dbs statement pdf extraction

--- cleaning/pdf-mining/pdf2txt.py
+++ cleaning/pdf-mining/pdf2txt.py
@@ -5,7 +5,7 @@ Converts PDF text content (though not images containing text) to plain text, htm
 """
 import io
 import json
-
+import pandas as pd
 import re
 
 import os
@@ -57,16 +57,19 @@ def dbs_table_parse(txt):
     all_records = []
     other_records = []
     for line in tmp_txt.split("\n\n"):
-        line = line.strip().replace("\n", "")
+        line = line.strip().replace("\n", " ")
         if re.search("REF NO :.", line):
-            line = re.sub("REF NO :.*$", "", line)
+            # line = re.sub("REF NO :.*$", "", line)
             line = re.sub('\s+', " ", line)
             all_records.append(line)
+            pass
         elif line is not "$":
             other_records.append(line)
     count_line = len(all_records)
     for i in range(count_line):
         result.append([other_records[i], all_records[i], other_records[count_line + i]])
+    # print(result)
+    # exit()
     return result
 
 
@@ -80,62 +83,47 @@ def uob_table_parse(txt, idx):
     data_record = []
     trans_record = []
     count_record = []
-    if idx == 1:
-        begin = re.search('Description of Transaction', txt).span()[1]
-        end = re.search('Contact Us', txt).span()[0]
-        for line in txt[begin:end].split("\n\n"):
-            line = line.strip().replace("\n", "")
-            if len(line) == 0:
-                continue
-            if re.search("Ref No", line):
-                line = re.sub("Ref No.*$", "", line)
-                line = re.sub('\s+', " ", line)
-            if re.search("^[0-9]{2} JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC$", line):
-                data_record.append(line)
-            else:
-                trans_record.append(line)
-        begin = re.search('Transaction Amount', txt).span()[1]
-        for line in txt[begin:].split("\n\n"):
-            line = line.strip().replace("\n", " ")
-            if re.search("SGD", line) or len(line) == 0:
-                continue
-            # if re.search("Ref No", line):
-            #     line = re.sub("Ref No.*$", "", line)
-            #     line = re.sub('\s+', " ", line)
+    lines = []
+    is_skip = True
+    for line in txt.split("\n\n"):
+        line = line.strip().replace("\n", " ")
+        if is_skip:
+            if re.search("Trans Date", line):
+                is_skip = False
+            continue
+        if re.search("--------------------------------------------------", line):
+            continue
+        lines.append(line)
+    for line in lines:
+        # print(line)
+        line = line.strip().replace("\n", " ")
+        if re.search("SGD|^UOB.*$|"
+                     "LU WEI|Trans Date|Post Date|Description of Transaction|Page [0-9]+ of [0-9]+",
+                     line) or len(line) == 0:
+            continue
+        if re.search("^[0-9]{2} JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC$", line) and len(line) == 6:
+            data_record.append(line)
+        elif re.search("^([+-]?)((\d{1,3}(,\d{3})*)|(\d+))(\.\d{2})?(\sCR)*$", line):
             count_record.append(line)
-        # print(data_record, trans_record, count_record)
-        # print(len(data_record), len(trans_record), len(count_record))
-        # exit()
-        date_idx = 0
-        for i in range(len(trans_record)):
-            if re.search("TOTAL|PREVIOUS|[^0-9a-zA-Z]+", trans_record[i]):
-                continue
-            result.append([data_record[2 * date_idx], trans_record[i], count_record[i]])
-            date_idx += 1
-    if idx == 2:
-        begin = re.search('Description of Transaction', txt)
-        if begin is None:
-            return result
         else:
-            begin = begin.span()[1]
-        for line in txt[begin:].split("\n\n"):
-            line = line.strip().replace("\n", " ")
-            if re.search("SGD|^UOB.*$|"
-                         "LU WEI|PostDate|TransDate|Description of Transaction|"
-                         "Page [0-9]+ of [0-9]+", line) or len(line) == 0:
-                continue
-            if re.search("^[0-9]{2} JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC$", line) and len(line) == 6:
-                data_record.append(line)
-            elif re.search("^([+-]?)((\d{1,3}(,\d{3})*)|(\d+))(\.\d{2})?(\sCR)*$", line):
-                count_record.append(line)
-            else:
-                trans_record.append(line)
-        date_idx = 0
-        for i in range(len(trans_record)):
-            if re.search("TOTAL|PREVIOUS|[^0-9a-zA-Z]+", trans_record[i]):
-                continue
-            result.append([data_record[2 * date_idx], trans_record[i], count_record[i]])
-            date_idx += 1
+            trans_record.append(line)
+    date_idx = 0
+    # print(data_record, trans_record, count_record)
+    # print(len(data_record), len(trans_record), len(count_record))
+    data_r = []
+    trans_r = []
+    for i in range(len(data_record) // 2):
+        if re.search("TOTAL|PREVIOUS", trans_record[i]):
+            continue
+        if re.search("CHOOSE ANY 5 TRANSACTIONS ", trans_record[i]):
+            break
+        data_r.append(data_record[2 * date_idx])
+        trans_r.append(trans_record[i])
+        date_idx += 1
+    for i in range(len(trans_r)):
+        result.append([data_r[i], trans_r[i], count_record.pop()])
+    # print(result)
+    # exit()
     return result
 
 
@@ -175,12 +163,28 @@ if __name__ == '__main__':
         if filename.endswith(".pdf"):
             txt = pdf2txt(dir + "/" + filename)
             result += dbs_parse(txt)
+            print(filename, len(result))
     # uob
     dir = "assets/uob"
     for filename in os.listdir(dir):
         if filename.endswith(".pdf"):
-            print(filename)
             txt = pdf2txt(dir + "/" + filename)
+            # print(filename)
             result += uob_parse(txt)
-    with open(dir + "/out.json", 'w') as outfile:
-        json.dump(result, outfile)
+            print(filename, len(result))
+            # print(result)
+    dic = {}
+    list_date = []
+    list_trans = []
+    list_count = []
+    for item in result:
+        list_date.append(item[0])
+        list_trans.append(item[1])
+        list_count.append(item[2])
+    dic["DATE"] = list_date
+    dic["TITLE"] = list_trans
+    dic["PRICE"] = list_count
+    df = pd.DataFrame(data=dic)
+    df.to_csv("out.csv", sep=',', index=False)
+    # with open(dir + "/out.json", 'w') as outfile:
+    #     json.dump(result, outfile)
