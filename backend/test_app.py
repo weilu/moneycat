03b5b5f98467a5b8192f02e153733a348680a1a2
Wei Lu <luwei.here@gmail.com>
2018-04-18 23:04:21 +0800

    Reservoir sample also returns remaining indexes

--- backend/test_app.py
+++ backend/test_app.py
@@ -5,6 +5,7 @@ from statistics import mean
 import pandas as pd
 
 MAX_INT = 1000
+SAMPLE_SIZE = 10
 population = list(range(1, MAX_INT+1))
 # random.shuffle(population)
 POP = pd.DataFrame(data={'x': population})
@@ -16,22 +17,30 @@ class TestApp(unittest.TestCase):
     def test_reservior_sampling(self):
         means = []
         for _ in range(REPEAT_TIMES):
-            means.append(mean(reservior_sampling(10, POP)['x']))
+            samples = reservior_sampling(SAMPLE_SIZE, POP)[0]
+            means.append(mean(samples['x']))
         # according to central limit theorem
         self.assertTrue(abs(mean(means) - MAX_INT/2) < THRESHOLD * (MAX_INT/2))
+
+    def test_reservior_sampling_remaining(self):
+        samples, remaining = reservior_sampling(SAMPLE_SIZE, POP)
+        self.assertEqual(len(samples), SAMPLE_SIZE)
+        self.assertEqual(len(remaining), MAX_INT - SAMPLE_SIZE)
 
     def test_reservior_sampling_with_existing_samples(self):
         means = []
         for _ in range(REPEAT_TIMES):
             k = 5
-            means.append(mean(reservior_sampling(10, POP.loc[k:], k, POP.loc[0:k-1])['x']))
+            samples = reservior_sampling(SAMPLE_SIZE, POP.loc[k:], k, POP.loc[0:k-1])[0]
+            means.append(mean(samples['x']))
         self.assertTrue(abs(mean(means) - MAX_INT/2) < THRESHOLD * (MAX_INT/2))
 
     def test_reservior_sampling_with_full_reservior(self):
         means = []
         for _ in range(REPEAT_TIMES):
             k = 10
-            means.append(mean(reservior_sampling(10, POP.loc[k:], k, POP.loc[0:k-1])['x']))
+            samples = reservior_sampling(SAMPLE_SIZE, POP.loc[k:], k, POP.loc[0:k-1])[0]
+            means.append(mean(samples['x']))
         self.assertTrue(abs(mean(means) - MAX_INT/2) < THRESHOLD * (MAX_INT/2))
 
 
