dd902a1f9cfcdbd1b783c070801c97a943331d79
Wei Lu <luwei.here@gmail.com>
2018-04-19 18:33:24 +0800

    Spit out accuracy & f1 scores

--- classification/train.py
+++ classification/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing, metrics
 from sklearn.linear_model import SGDClassifier
 from sklearn.externals import joblib
@@ -9,6 +10,7 @@ from glob import glob
 import boto3
 import os
 import pickle
+from statistics import mean
 
 s3 = boto3.client('s3')
 
@@ -17,29 +19,31 @@ def read_data():
     return pd.read_csv('../cleaning/pdf-mining/out_wei_labelled_full.csv', sep=",")
 
 
-def cross_validate(X, y):
+def cross_validate(X, y, classifier):
     fold = 10
-    sum = 0
     kf = KFold(n_splits=fold, shuffle=True, random_state=42)
-    classifier, transformer = None, None
+    accuracies = []
+    f1s = []
     for train_index, test_index in kf.split(X):
-        classifier, transformer = train(X[train_index], y[train_index])
+        transformer = train(X[train_index], y[train_index], classifier)
         X_test = transformer.transform(X[test_index])
         pred = classifier.predict(X_test)
-        score = metrics.accuracy_score(y[test_index], pred)
-        print("test sample size: %d, accuracy: %0.3f" % (X_test.shape[0], score))
-        sum += score
-    average = sum/fold
-    print(f'{classifier} produces an accuracy of {average}')
-    return (classifier, transformer)
+        accuracy = metrics.accuracy_score(y[test_index], pred)
+        f1 = metrics.f1_score(y[test_index], pred, average='weighted')
+        print("test sample size: %d, accuracy: %0.3f, f1 score: %0.3f" \
+                % (X_test.shape[0], accuracy, f1))
+        accuracies.append(accuracy)
+        f1s.append(f1)
+    accuracy = mean(accuracies)
+    print('%s produces an accuracy of %0.3f, and f1 score of %0.3f'\
+            % (classifier.__class__.__name__, accuracy, mean(f1s)))
+    return (classifier, accuracy)
 
 
-def train(X, y):
+def train(X, y, classifier):
     max_df = 1.0
     transformer = TfidfVectorizer(max_df=max_df)
     X_train = transformer.fit_transform(X)
-    # classifier = MultinomialNB(alpha=.01) # TODO: try partial fit
-    classifier = SGDClassifier(random_state=123, max_iter=1000, tol=1e-3)
     classifier.fit(X_train, y)
 
     print("train n_samples: %d, n_features: %d" % X_train.shape)
@@ -47,7 +51,7 @@ def train(X, y):
         print("idf stop words: ")
         print(" ".join(transformer.stop_words_))
 
-    return (classifier, transformer)
+    return transformer
 
 
 def export_model(classifier, transformer, label_trasformer, test_samples,
@@ -69,6 +73,15 @@ def export_model(classifier, transformer, label_trasformer, test_samples,
         print(obj['Key'], obj['LastModified'])
 
 
+# Return model with the highest accuracy
+def test_models(X, y):
+    classifiers = [MultinomialNB(alpha=.01),
+                   KNeighborsClassifier(),
+                   SGDClassifier(random_state=123, max_iter=1000, tol=1e-3)]
+    classifier_n_accuracies = map(lambda c: cross_validate(X, y, c), classifiers)
+    return sorted(classifier_n_accuracies, key=lambda pair: pair[1], reverse=True)[0]
+
+
 if __name__ == '__main__':
     personal_data = read_data()
     print("data size:", personal_data.shape)
@@ -77,12 +90,13 @@ if __name__ == '__main__':
     y = le.fit_transform(personal_data['category'])
     X = personal_data['description']
     # report cross validation accuracy
-    cross_validate(X, y)
+    classifier, accuracy = test_models(X, y)
+    print('Using {} with accuracy {}'.format(classifier, accuracy))
 
     # leave out a small test subset for benchmarking
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=50,
-                                                        random_state=42)
-    classifier, transformer = train(X_train, y_train)
+                                                        random_state=123)
+    transformer = train(X_train, y_train, classifier)
     pred = classifier.predict(transformer.transform(X_test))
     report = metrics.classification_report(y_test, pred, target_names=list(le.classes_))
     print(report)
